b5979c38995fba3510a2ef894a264b4b0bf3b47a
Jonah Robinson <jonahtomrobinson@gmail.com>
2019-02-24 13:48:59 +0000

    Added pages for each nav bar option

---
+++ app/src/main/java/com/example/climblog/IndoorFragment.kt
@@ -0,0 +1,21 @@
+package com.example.climblog
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class IndoorFragment : Fragment() {
+    companion object {
+        fun newInstance(): IndoorFragment {
+            return IndoorFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_indoor, container, false)
+    }
+}
